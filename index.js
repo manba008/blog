855cc4384d94c0745e3a8a2b9d3f9640bf5acf25
anan <shouce100@qq.com>
2024-06-02 19:07:29 +0800

    1907

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var button_js = document.getElementById('myButtonJs');
  
  // 为按钮添加点击事件监听
  button_js.addEventListener('click', function() {
-     alert('按钮被点击了js!');
+     alert('按钮被点击了js111!');
  });
 
